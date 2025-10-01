bc9ff9136fc60dea451f274a0a2e340fbdd66839
Sean McLeaish <smcleaish@gmail.com>
2025-10-01 12:28:44 -0400

    secret message function

--- secret_message.py
+++ secret_message.py
@@ -1,4 +1,3 @@
-
 print("Secret Message Generator")
 print()
 guest_name = input("Enter the recipient's name: ").strip()
@@ -34,3 +33,43 @@ if user_accept == "Y":
     print("Awesome! Enjoy the party!")
 else:
     print("Sorry, we couldn't help you")
+
+
+def secret_message_generator():
+    print("Secret Message Generator \n")
+    guest_name = input("Enter the recipient's name: ").strip()
+    message = input("Enter your secret party message: ").strip()
+    cleaned_message = message.strip()
+    uppercase_version = cleaned_message.upper()
+    reverse_version = cleaned_message[::-1]
+    char_count = len(cleaned_message)
+    print()
+    print("Which format of your message would you like to see?")
+    print(f"1) Uppercase message: {uppercase_version}")
+    print(f"2) Reversed message: {reverse_version}")
+    print(f"3) Number of characters: {char_count}")
+    print()
+    user_choice = input(
+        "Please enter 1, 2, or 3 to choose the format of your message: "
+    )
+    print()
+    if user_choice == "1":
+        print(f"Hey {guest_name}, here's your secret code: {uppercase_version}")
+    elif user_choice == "2":
+        print(f"Hey {guest_name}, here's your secret code: {reverse_version}")
+    elif user_choice == "3":
+        print(f"Hey {guest_name}, here's your secret code: {char_count}")
+    else:
+        print("Please enter 1, 2, or 3")
+    print()
+    print("Do you accept this message?")
+    print("Y) Yes")
+    print("N) No")
+    user_accept = input("Enter Y for Yes. Enter N for No: ")
+    if user_accept == "Y":
+        print("Awesome! Enjoy the party!")
+    else:
+        secret_message_generator()
+
+
+secret_message_generator()
--- src/secret_message.py
+++ src/secret_message.py
@@ -12,7 +12,8 @@ class SecretMessage:
     def create(cls, msg: str, name: str) -> Self:
         return cls(msg=msg, name=name, clean=msg.strip())
 
-    def make_secret(self) -> str:
+    @property
+    def secret(self) -> str:
         return "".join(reversed(self.clean.upper()))
 
 
@@ -25,7 +26,7 @@ def get_info() -> tuple[str, str]:
 def new_secret_message() -> str:
     msg, name = get_info()
     message = SecretMessage.create(msg, name)
-    return message.make_secret()
+    return message.secret
 
 
 print(new_secret_message())
