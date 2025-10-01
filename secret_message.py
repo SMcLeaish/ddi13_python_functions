ef8e550afa1213b00813b612b1a29eb159e6d40a
djkraase <djkraase@protonmail.com>
2025-10-01 10:04:09 -0400

    Updated secret message generator

--- secret_message.py
+++ secret_message.py
@@ -1,34 +1,34 @@
 
-print("Secret Message Generator")
+print("Secret Message Generator\n")
+
+guest_name = input("Enter the recipient's name: \n").strip()
 print()
-guest_name = input("Enter the recipient's name: ").strip()
-print()
-message = input("Enter your secret party message: ").strip()
+message = input("Enter your secret party message: \n").strip()
 
 cleaned_message = message.strip()
 uppercase_version = cleaned_message.upper()
 reverse_version = cleaned_message[::-1]
 char_count = len(cleaned_message)
 print()
-print("Which format of your message would you like to see?")
+print("Which format of your message would you like to see?\n")
 print(f"1) Uppercase message: {uppercase_version}")
 print(f"2) Reversed message: {reverse_version}")
 print(f"3) Number of characters: {char_count}")
 print()
-user_choice = input("Please enter 1, 2, or 3 to choose the format of your message: ")
+user_choice = input("Please enter 1, 2, or 3 to choose the format of your message: \n")
 print()
 if user_choice == "1":
-    print(f"Hey {guest_name}, here's your secret code: {uppercase_version}")
+    print(f"Hey {guest_name}, here's your secret code: {uppercase_version}\n")
 elif user_choice == "2":
-    print(f"Hey {guest_name}, here's your secret code: {reverse_version}")
+    print(f"Hey {guest_name}, here's your secret code: {reverse_version}\n")
 elif user_choice == "3":
-    print(f"Hey {guest_name}, here's your secret code: {char_count}")
+    print(f"Hey {guest_name}, here's your secret code: {char_count}\n")
 else:
-    print("Please enter 1, 2, or 3")
-print()
+    print("Please enter 1, 2, or 3\n")
+
 print("Do you accept this message?")
 print("Y) Yes")
-print("N) No")
+print("N) No\n")
 user_accept = input("Enter Y for Yes. Enter N for No: ")
 if user_accept == "Y":
     print("Awesome! Enjoy the party!")
